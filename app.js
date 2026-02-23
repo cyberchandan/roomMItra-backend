8dec282afbb00769c125527bcb9e347301a871d7
Chandan Kumar <chandan36024@gmail.com>
2026-02-23 11:32:24 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const cors=require('cors')
+require("dotenv").config();
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    console.log("working / request")
+    res.send("working ")
+
+})
+
+const PORT= process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log(`server running on ${PORT} port`)
+})
