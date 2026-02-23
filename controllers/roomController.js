32622d1883e5348d89474ec4fd34f013d73d2f04
Chandan Kumar <chandan36024@gmail.com>
2026-02-23 14:18:31 +0530

    search by filter added

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -19,12 +19,34 @@ exports.createRoom = async (req, res) => {
 
 exports.getAllRooms = async (req, res) => {
   try {
-    const rooms = await Room.find()
-      .populate("owner", "name email");   // password automatically include nahi hoga
+    let filter = {};
+
+    const { city, listingType, minPrice, maxPrice } = req.query;
+
+    // City filter
+    if (city) {
+      filter.city = city;
+    }
+
+    // Listing Type filter
+    if (listingType) {
+      filter.listingType = listingType;
+    }
+
+    // Price filter
+    if (minPrice && maxPrice) {
+      filter.price = {
+        $gte: Number(minPrice),
+        $lte: Number(maxPrice),
+      };
+    }
+
+    const rooms = await Room.find(filter).populate("owner", "name email");
 
     res.status(200).json(rooms);
+
   } catch (error) {
-    console.log("GET ROOMS ERROR:", error);
+    console.log("FILTER ERROR:", error);
     res.status(500).json({
       message: "Server error",
       error: error.message,
